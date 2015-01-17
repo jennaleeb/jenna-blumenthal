44c3d808a32ebfae4e331da7e0783a733769437e
jennaleeb <jennaleeblumenthal@gmail.com>
2015-01-17 16:02:05 -0500

    Beginning to make mobile friendly

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,5 +3,11 @@
 (function($) {
   $(document).ready(function() {
     // Add your jQuery code here
+    $(".timeline-item").hover(function () {
+    $(".timeline-item").removeClass("active");
+    $(this).toggleClass("active");
+    $(this).prev(".timeline-item").toggleClass("close");
+    $(this).next(".timeline-item").toggleClass("close");
+});
   });
 })(jQuery);
